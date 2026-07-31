426e5314a9fdfbce476ad73d9fd681daabaf3552
angleroofingexterior-ctrl <angleroofingexterior@gmail.com>
2026-07-31 17:52:41 -0500

    Refresh Project 220 admin portal cache

--- public-github/sw.js
+++ public-github/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "project-220-v3";
+const CACHE_NAME = "project-220-admin-v4";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
@@ -18,16 +18,14 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))),
-  );
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
   event.respondWith(
-    fetch(event.request)
+    fetch(event.request, { cache: "no-store" })
       .then((response) => {
         if (response.ok && new URL(event.request.url).origin === self.location.origin) {
           const copy = response.clone();
